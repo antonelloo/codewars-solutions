6c43158a98e0074a8a879d89f2f6786e1d175b8c
unknown <aorozco18@yahoo.com>
2022-03-11 12:25:57 -0800

    completed codewars 012

---
+++ js/solution012.js
@@ -0,0 +1,15 @@
+// Write a function which converts the input string to uppercase.
+
+
+
+function makeUpperCase(str) {
+    return str.toUpperCase()
+     }
+
+    
+     
+makeUpperCase('one') // "One"
+makeUpperCase('two') // "Two"
+makeUpperCase('three') // "Three"
+
+    
