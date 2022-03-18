f3acac05fdc0cfc0800cd7d2322ef742be545e85
unknown <aorozco18@yahoo.com>
2022-03-18 14:04:04 -0700

    completed codewars 019

---
+++ js/solution019.js
@@ -0,0 +1,27 @@
+// Introduction
+// The first century spans from the year 1 up to and including the year 100, 
+// the second century - from the year 101 up to and including the year 200, 
+// etc.
+
+// Task
+// Given a year, return the century it is in.
+
+// Examples
+// 1705 --> 18
+// 1900 --> 19
+// 1601 --> 17
+// 2000 --> 20
+
+
+
+//SOLUTION
+
+function century(year) {
+    let calc = (year/100)
+    let round = Math.ceil(calc)
+    return round
+  }
+
+
+century(1705) //18
+century(1900) //19
