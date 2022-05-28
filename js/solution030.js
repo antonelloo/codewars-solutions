38a66d7f4159527b1c97300f1d3fa46c28076909
unknown <aorozco18@yahoo.com>
2022-05-27 19:04:50 -0700

    completed codewars 030

---
+++ js/solution030.js
@@ -0,0 +1,22 @@
+// Fix the function
+// I created this function to add five to any number that was passed in to it and 
+// return the new value. It doesn't throw any errors but it returns the wrong 
+// number.
+
+// Can you help me fix the function?
+
+
+
+
+//SOLUTION
+
+function addFive(num) {
+    var total = num + 5
+    return total
+  }
+
+
+
+  addFive(5) //10
+  addFive(10) // 15
+  addFive(-15) //-10
