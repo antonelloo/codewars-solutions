9f79d21b9d0f23b5bff3ae4bb9bc3f5318bf1dd4
unknown <aorozco18@yahoo.com>
2022-05-29 19:25:55 -0700

    completed codewars 032

---
+++ js/solution032.js
@@ -0,0 +1,18 @@
+// Grasshopper - Function syntax debugging
+
+// A student was working on a function and made some syntax mistakes 
+// while coding. Help them find their mistakes and fix them.
+
+
+
+//SOLUTION
+
+function main (verb, noun) {
+    return verb + noun
+  }
+
+
+  
+
+  main('take ', 'item') //'take item'
+  main('use ', 'sword') //'use sword')
