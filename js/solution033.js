20cc1a4e4f6d68c7521fbeafe9eb6f6bcc981324
unknown <aorozco18@yahoo.com>
2022-05-30 19:31:42 -0700

    completed codewars 033

---
+++ js/solution033.js
@@ -0,0 +1,16 @@
+// Create a function that accepts a list/array and a number n, and returns a
+//  list/array of the first n elements from the list/array.
+
+
+
+
+//SOLUTION
+
+function take(arr, n) {
+    return arr.splice(0, n)
+  }
+
+
+
+  take([0, 1, 2, 3, 5, 8, 13], 3) //[0, 1, 2]
+
