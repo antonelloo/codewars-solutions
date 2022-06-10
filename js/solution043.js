7463c49491f08a724589325788eb437100f3dd50
unknown <aorozco18@yahoo.com>
2022-06-10 11:32:42 -0700

    completed codewars 044

--- js/solution043.js
+++ js/solution043.js
@@ -8,8 +8,6 @@
 // var sleigh = new Sleigh();
 // sleigh.authenticate("Santa Claus", "Ho Ho Ho!"); // must return TRUE
 
-// sleigh.authenticate("Santa", "Ho Ho Ho!"); // must return FALSE
-// sleigh.authenticate("Santa Claus", "Ho Ho!"); // must return FALSE
 // sleigh.authenticate("jhoffner", "CodeWars"); // Nope, even Jake is not allowed to use the sleigh ;)
 
 
@@ -20,7 +18,7 @@ function Sleigh() {}
 Sleigh.prototype.authenticate = function(name, password) {
   return name === "Santa Claus" && password ===  "Ho Ho Ho!"
 };
-  
-mergeArrays([1,2,3,4], [5,6,7,8]) //[1,2,3,4,5,6,7,8]
-mergeArrays([1,3,5,7,9], [10,8,6,4,2]) //[1,2,3,4,5,6,7,8,9,10]
-mergeArrays([1,3,5,7,9,11,12], [1,2,3,4,5,10,12])  //[1,2,3,4,5,7,9,10,11,12]
+
+
+// sleigh.authenticate("Santa", "Ho Ho Ho!"); // must return FALSE
+// sleigh.authenticate("Santa Claus", "Ho Ho!"); // must return FALSE
