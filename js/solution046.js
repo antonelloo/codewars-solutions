29263a2bffe492b72acacb6ea9b2c24f9dc55ea9
unknown <aorozco18@yahoo.com>
2022-06-27 12:20:41 -0700

    completed codewars 046

---
+++ js/solution046.js
@@ -0,0 +1,52 @@
+// DESCRIPTION:
+
+// Complete the function that receives as input a string, and produces outputs according to the following table:
+
+// Input	Output
+// "Jabroni"	"Patron Tequila"
+// "School Counselor"	"Anything with Alcohol"
+// "Programmer"	"Hipster Craft Beer"
+// "Bike Gang Member"	"Moonshine"
+// "Politician"	"Your tax dollars"
+// "Rapper"	"Cristal"
+// anything else	"Beer"
+// Note: anything else is the default case: if the input to the function is not any of the values in the table, then the return value should be "Beer".
+
+// Make sure you cover the cases where certain words do not show up with correct capitalization. For example, the input "pOLitiCIaN" should still return "Your tax dollars".
+
+
+
+
+
+
+
+//SOLUTION #1
+
+function getDrinkByProfession(param){
+    const str = param.toLowerCase()
+    return str === "jabroni" ? "Patron Tequila" : str === "school counselor" ? "Anything with Alcohol" :  str === "programmer" ? "Hipster Craft Beer" :  str === "bike gang member" ? "Moonshine" :  str === "politician" ? "Your tax dollars" :  str === "rapper" ? "Cristal" : "Beer"  
+    }
+
+
+//SOLUTION #2
+
+
+    function getDrinkByProfession(param) {
+        param = param.toLowerCase();
+        
+        switch(param) {
+          case "jabroni": return "Patron Tequila";
+          case "school counselor": return "Anything with Alcohol";
+          case "programmer": return "Hipster Craft Beer";
+          case "bike gang member": return "Moonshine";
+          case "politician": return "Your tax dollars";
+          case "rapper": return "Cristal";
+          default: return "Beer";
+        }
+      }
+
+
+
+getDrinkByProfession('jabroni') //"Patron Tequila"
+getDrinkByProfession("Politician") //"Your tax dollars"
+getDrinkByProfession("School Counselor") //"Anything with Alcohol"
