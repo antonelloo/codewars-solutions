9a146572913fc586063a75f24d25667047c593f6
unknown <aorozco18@yahoo.com>
2022-12-05 17:27:34 -0800

    completed codewars 158

---
+++ js/solution158.js
@@ -0,0 +1,41 @@
+// ---DESCRIPTION:
+/* 
+
+6 kyu --- Counting Duplicates
+
+Count the number of Duplicates
+Write a function that will return the count of distinct case-insensitive alphabetic characters and numeric digits that occur more than once in the input string. The input string can be assumed to contain only alphabets (both uppercase and lowercase) and numeric digits.
+
+Example
+"abcde" -> 0 # no characters repeats more than once
+"aabbcde" -> 2 # 'a' and 'b'
+"aabBcde" -> 2 # 'a' occurs twice and 'b' twice (`b` and `B`)
+"indivisibility" -> 1 # 'i' occurs six times
+"Indivisibilities" -> 2 # 'i' occurs seven times and 's' occurs twice
+"aA11" -> 2 # 'a' and '1'
+"ABBA" -> 2 # 'A' and 'B' each occur twice
+
+*/
+
+
+
+//---SOLUTION:
+function duplicateCount(text){
+    let arrayOfLetters = text.toLowerCase().split("")
+    let total = []
+      for(const letter of arrayOfLetters){
+        if(arrayOfLetters.indexOf(letter) !== arrayOfLetters.lastIndexOf(letter)){
+          total.push(letter)
+    }
+  }
+    return new Set(total).size 
+  }
+
+
+// ---RESULTS:
+duplicateCount("")// 0
+duplicateCount("abcde")// 0
+duplicateCount("aabbcde")// 2
+duplicateCount("aabBcde")// 2,"should ignore case"
+duplicateCount("Indivisibility")// 1
+duplicateCount("Indivisibilities")// 2, "characters may not be adjacent"
