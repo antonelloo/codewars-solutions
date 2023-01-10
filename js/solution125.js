5e46816daa0b173be7017596d2ca7b1d52123a11
unknown <aorozco18@yahoo.com>
2023-01-09 17:50:39 -0800

    completed codewars 125

---
+++ js/solution125.js
@@ -0,0 +1,22 @@
+// ---DESCRIPTION:
+/* 
+
+7 kyu --- Largest Square Inside A Circle
+
+Determine the area of the largest square that can fit inside a circle with radius r.
+
+*/
+
+
+
+//---SOLUTION:
+function areaLargestSquare(r) {
+    return (r*2) * (r*2) / 2 
+  }
+    
+  
+
+// ---RESULTS:
+areaLargestSquare(5)// 50
+areaLargestSquare(7)// 98
+areaLargestSquare(15)// 450
