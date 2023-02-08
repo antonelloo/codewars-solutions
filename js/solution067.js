bb8e2513ad826830935bebc3eef8838ef6b5a77a
unknown <aorozco18@yahoo.com>
2023-02-07 18:34:23 -0800

    completedcodewars 071

--- js/solution067.js
+++ js/solution067.js
@@ -1,24 +1,8 @@
-// DESCRIPTION:
-// You are given a string of letters and an array of numbers.
-// The numbers indicate positions of letters that must be removed, in order, starting from the beginning of the array.
-// After each removal the size of the string decreases (there is no empty space).
-// Return the only letter left.
-
-// Example:
-
-// let str = "zbk", arr = [0, 1]
-//     str = "bk", arr = [1]
-//     str = "b", arr = []
-//     return 'b'
-// Notes
-// The given string will never be empty.
-// The length of the array is always one less than the length of the string.
-// All numbers are valid.
-// There can be duplicate letters and numbers.
+// --DESCRIPTION:
 
 
 
-// --Solution--
+// --SOLUTION:
 
 function lastSurvivor(letters, array) {
   const lettersArray = letters.split("")
@@ -32,6 +16,6 @@ function lastSurvivor(letters, array) {
 }
 
 
-// --Results--
+// --RESULTS--
 lastSurvivor('kbc', [0, 1]) //Result: "b"
 lastSurvivor('abc', [1, 1]) //Result: "a"
