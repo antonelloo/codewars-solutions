2886009bb1eb2031c9b58104e4884bca5338c42d
unknown <aorozco18@yahoo.com>
2023-02-25 13:32:21 -0800

    completed codewars 112

---
+++ js/solution112.js
@@ -0,0 +1,44 @@
+// ---DESCRIPTION:
+/* 
+
+6 kyu --- Your order, please
+
+Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+Examples
+"is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+"4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+""  -->  ""
+
+*/
+
+
+
+//---SOLUTION:
+function order(string){
+    if(string === ""){
+      return ""
+    }else {
+        const array = string.split(" ")
+        const arrayLength = array.length +1 
+        let newArray = []
+  
+          for(let i=0; i<arrayLength; i++){
+            for(let j=0; j<array.length; j++){
+              if(array[j].includes(i)){
+                newArray.push(array[j])
+              }
+            }
+          }
+      return newArray.join(" ")
+    }}
+
+
+// ---RESULTS:
+order("is2 Thi1s T4est 3a")// "Thi1s is2 3a T4est"
+order("4of Fo1r pe6ople g3ood th5e the2")//"Fo1r the2 g3ood 4of th5e pe6ople"
+order("")// ""
