61a93e4a4655e7446b32faece4e49f665b4e7c8f
unknown <aorozco18@yahoo.com>
2023-04-27 15:04:48 -0700

    completed codewars 150

---
+++ js/solution150.js
@@ -0,0 +1,28 @@
+// ---DESCRIPTION:
+/* 
+
+8 kyu --- Multiply the number  
+
+Jack really likes his number five: the trick here is that you have to multiply each number by 5 raised to the number of digits of each numbers, so, for example:
+
+multiply(3)==15 // 3 * 5¹
+multiply(10)==250 // 10 * 5²
+multiply(200)==25000 // 200 * 5³
+multiply(0)==0 // 0 * 5¹
+multiply(-3)==-15 // -3 * 5¹
+
+*/
+
+
+
+//---SOLUTION:
+function multiply(number){
+    return number * Math.pow(5, String(number).replace("-", "").length)
+  }
+
+
+
+// ---RESULTS:
+multiply(5) //25
+multiply(200) //25000
+multiply(0) //0
