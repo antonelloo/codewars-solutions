36e26009b86885a41f9909a06033826d034233c3
unknown <aorozco18@yahoo.com>
2023-05-08 14:55:58 -0700

    completed codewars 159

---
+++ js/solution159.js
@@ -0,0 +1,26 @@
+// ---DESCRIPTION:
+/* 
+
+7 kyu --- Remove consecutive duplicate words
+
+Your task is to remove all consecutive duplicate words from a string, leaving only first words entries. For example:
+
+"alpha beta beta gamma gamma gamma delta alpha beta beta gamma gamma gamma delta"
+
+--> "alpha beta gamma delta alpha beta gamma delta"
+Words will be separated by a single space. There will be no leading or trailing spaces in the string. An empty string (0 words) is a valid input.
+
+*/
+
+
+
+//---SOLUTION:
+function removeConsecutiveDuplicates(string) {
+    return string.split(" ").filter((word, index, array) => word !== array[index-1] ? word : null).join(" ")
+  }
+
+
+// ---RESULTS:
+removeConsecutiveDuplicates("alpha alphabeta alphagamma") // "alpha alphabeta alphagamma
+removeConsecutiveDuplicates("alpha alpha beta alpha alpha") // "alpha beta alpha
+removeConsecutiveDuplicates("alpha beta beta") // "alpha beta"
