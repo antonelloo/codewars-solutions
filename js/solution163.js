026ad3891c0f7ac4ce57d76fdd58f301223f2318
unknown <aorozco18@yahoo.com>
2023-05-12 16:07:02 -0700

    completed codewars 163

---
+++ js/solution163.js
@@ -0,0 +1,40 @@
+// ---DESCRIPTION:
+/* 
+
+7 kyu --- Find the Capitals
+
+Complete the method that takes a sequence of objects with two keys each: country or state, and capital. Keys may be symbols or strings.
+
+The method should return an array of sentences declaring the state or country and its capital.
+
+Examples
+state_capitals = [{state: 'Maine', capital: 'Augusta'}]
+capital(state_capitals)[0] // returns "The capital of Maine is Augusta"
+
+country_capitals = [{'country' : 'Spain', 'capital' : 'Madrid'}]
+capital(country_capitals)[0]  // returns "The capital of Spain is Madrid"
+
+mixed_capitals: [{"state" : 'Maine', capital: 'Augusta'}, {country: 'Spain', "capital" : "Madrid"}]
+capital(mixed_capitals)[1] // returns "The capital of Spain is Madrid"
+
+*/
+
+
+
+//---SOLUTION:
+function capital(capitals){
+    return capitals.map(obj => {
+      return `The capital of ${obj.state || obj.country} is ${obj.capital}`
+    })      
+}
+
+
+// ---RESULTS:
+state_capitals = [{state: 'Maine', capital: 'Augusta'}]
+capital(state_capitals)[0] // "The capital of Maine is Augusta"
+
+country_capitals = [{'country' : 'Spain', 'capital' : 'Madrid'}]
+capital(country_capitals)[0] // "The capital of Spain is Madrid"
+
+mixed_capitals = [{"state" : 'Maine', capital: 'Augusta'}, {country: 'Spain', "capital" : "Madrid"}]
+capital(mixed_capitals)[1] // "The capital of Spain is Madrid"
