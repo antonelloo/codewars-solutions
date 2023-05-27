bd53092e1754641421c5548c0fe525a6a7346072
unknown <aorozco18@yahoo.com>
2023-05-27 16:48:08 -0700

    completed codewars 176

---
+++ js/solution176.js
@@ -0,0 +1,21 @@
+// ---DESCRIPTION:
+/* 
+
+7 kyu --- Reverse List
+
+Write reverseList function that simply reverses lists.
+
+*/
+
+
+
+//---SOLUTION:
+function reverseList(arr) {
+    return arr.reverse();
+  }
+
+
+// ---RESULTS:
+reverseList([]) // []
+reverseList([5,10,15,20]) // [20,15,10,5]
+reverseList([1,2,3]) // [3,2,1]
