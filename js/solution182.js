61917a929c9399531d7d93fa5c4eda36de4602aa
unknown <aorozco18@yahoo.com>
2023-06-04 16:49:01 -0700

    completed codewars 182

---
+++ js/solution182.js
@@ -0,0 +1,25 @@
+// ---DESCRIPTION:
+/* 
+
+7 kyu --- Filter Long Words
+
+Write a function that takes a string and an an integer n as parameters and returns a list of all words that are longer than n.
+
+Example:
+
+* With input "The quick brown fox jumps over the lazy dog", 4
+* Return ['quick', 'brown', 'jumps']
+
+*/
+
+
+
+//---SOLUTION:
+function filterLongWords(sentence, n) {
+    return sentence.split(" ").filter(word => word.length > n)
+}
+
+
+
+// ---RESULTS:
+filterLongWords("The quick brown fox jumps over the lazy dog", 4) //['quick', 'brown', 'jumps']
