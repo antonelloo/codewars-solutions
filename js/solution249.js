850a4c944d331205747a7b7922d0cab26d748541
unknown <aorozco18@yahoo.com>
2023-10-23 20:30:37 -0700

    completed codewars 249

---
+++ js/solution249.js
@@ -0,0 +1,34 @@
+/* 
+
+7 kyu --- Find the calculation type
+
+You have to create a function calcType, which receives 3 arguments: 2 numbers, and the result of an unknown operation performed on them (also a number).
+
+Based on those 3 values you have to return a string, that describes which operation was used to get the given result.
+
+The possible return strings are: "addition", "subtraction", "multiplication", "division".
+
+Example:
+
+calcType(1, 2, 3) -->   1 ? 2 = 3   --> "addition"
+
+Notes
+-In case of division you should expect that the result of the operation is obtained by using / operator on the input values - no manual data type conversion or rounding should be performed.
+-Cases with just one possible answers are generated.
+-Only valid arguments will be passed to the function.
+
+Only valid arguments will be passed to the function!
+*/
+
+
+
+//---SOLUTION:
+function calcType(a, b, res) {
+    return a + b === res ? 'addition' : a - b === res ? 'subtraction' : a * b === res ? 'multiplication' : 'division' 
+  }
+
+  
+// ---RESULTS:
+calcType(10,4,40) //'multiplication'); 
+calcType(10,5,5) //'subtraction'); 
+calcType(9,5,1.8) //'division'); 
